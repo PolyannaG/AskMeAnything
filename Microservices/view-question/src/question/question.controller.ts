2edce2d52aa5a63b19a1d3e756ff06beca439328
PolyannaG <poll.go@hotmail.com>
2021-05-16 16:37:57 +0300

    more endpoints added

--- Microservices/view-question/src/question/question.controller.ts
+++ Microservices/view-question/src/question/question.controller.ts
@@ -1,52 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { QuestionService } from './question.service';
-import { CreateQuestionDto } from './dto/create-question.dto';
-import { UpdateQuestionDto } from './dto/update-question.dto';
 
 @Controller('view_question')
 export class QuestionController {
   constructor(private readonly questionService: QuestionService) {}
 
-  /*
-  @Post()
-  create(@Body() createQuestionDto: CreateQuestionDto) {
-    return this.questionService.create(createQuestionDto);
-  }
-  */
+
   @Get('all/:date_from')
   findAll(@Param('date_from') date_from: Date) {
-    return this.questionService.findAll(date_from);
+    return this.questionService.findAll(date_from)
   }
+
+  @Get('all/:date_from/:Userid')
+  findAllUser(@Param('date_from') date_from: Date, @Param('Userid') Userid : number) {
+  return this.questionService.findAllUser(date_from, Userid)
+}
 
   @Get('id/:id')
-  findOne(@Param('id') id: string) {
-    return this.questionService.findOne(+id);
+  findOne(@Param('id') id: number) {
+    return this.questionService.findOne(+id)
   }
-  /*
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateQuestionDto: UpdateQuestionDto) {
-    return this.questionService.update(+id, updateQuestionDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.questionService.remove(+id);
-  }
-   */
-
-
 
   @Get('keywords')
-  findByKeywords(){
-    return this.questionService.findByKeywords();
+  findByKeywords() {
+    return this.questionService.findByKeywords()
   }
+
 
   @Get('keywords_user/:Userid')
-  findByKeywordsUser(@Param('Userid') Userid : number){
-    return this.questionService.findByKeywordsUser(Userid);
+  findByKeywordsUser(@Param('Userid') Userid : number)  {
+    return this.questionService.findByKeywordsUser(Userid)
   }
 
+  @Get('per_day')
+    showPerDay(){
+      return this.questionService.showPerDay()
+  }
 
+  @Get('per_day_user/:Userid')
+  showPerDayUser(@Param('Userid') Userid : number){
+      return this.questionService.showPerDayUser(Userid)
+  }
 
+  @Get('most_popular')
+  getMostPopular(){
+      return this.questionService.getMostPopular()
+  }
 
 }
--- Microservices/view-question/src/question/question.service.ts
+++ Microservices/view-question/src/question/question.service.ts
@@ -1,40 +1,40 @@
 import {Injectable, NotFoundException} from '@nestjs/common';
-import { CreateQuestionDto } from './dto/create-question.dto';
-import { UpdateQuestionDto } from './dto/update-question.dto';
 import {InjectEntityManager, InjectRepository} from "@nestjs/typeorm";
-import {createQueryBuilder, EntityManager, getConnection, getRepository, LessThan, MoreThan, Repository} from "typeorm";
+import {createQueryBuilder, EntityManager, LessThan,  Repository} from "typeorm";
 import {Question} from "./entities/question.entity";
 import {Keyword} from "./entities/keyword.entity";
+import {addMonths} from 'date-fns'
+
 
 @Injectable()
 export class QuestionService {
 
-  constructor(@InjectEntityManager() private manager : EntityManager, @InjectRepository(Keyword) private readonly keywordRepository : Repository<Keyword>) {}
-
-  /*
-  create(createQuestionDto: CreateQuestionDto) {
-    return 'This action adds a new question';
-  }
-   */
+  constructor(@InjectEntityManager() private manager : EntityManager,
+              @InjectRepository(Keyword) private readonly keywordRepository : Repository<Keyword>,
+             ) {}
 
   async findAll(date_from:Date) : Promise<Question[]>{
-    return this.manager.find(Question,{where: {date_created : LessThan(date_from)}, take: 10});
+   const questions= await this.manager.find(Question,{where: {date_created : LessThan(date_from)}, take: 10, relations:["keywords"]});
+   if (!questions || questions.length==0)
+     throw new NotFoundException(`No questions earlier than date ${date_from} found.`)
+    return questions
+  }
+
+  async findAllUser(date_from:Date, Userid:number) : Promise<Question[]>{
+    const questions= await this.manager.find(Question,{where: {date_created : LessThan(date_from), Userid: Userid}, take: 10, relations:["keywords"]});
+    if (!questions || questions.length==0)
+      throw new NotFoundException(`No questions earlier than date ${date_from} for user with id ${Userid} found.`)
+    return questions
   }
 
   async findOne(id: number) : Promise<Question> {
     const question=await this.manager.findOne(Question, id, {relations:["keywords"]})
-    //const question= this.manager.find(Keyword,{relations:["questions"]});
     if (!question)
       throw new NotFoundException(`Question with id ${id} not found.`)
     return question
   }
 
   async findByKeywords(): Promise<Object[]>{
-   /* const keywrd=await this.manager.findOne(Keyword,keyword, {relations:["questions"]})
-    if (!keywrd)
-      throw new NotFoundException(`No questions with keyword "${keyword}" found.`)
-    return keywrd.questions
-    */
     const quest= await this.keywordRepository.createQueryBuilder("Keyword").leftJoin("Keyword.questions", "questions").loadRelationCountAndMap('Keyword.questionCount', 'Keyword.questions').take(10).getMany()
     if (!quest || !quest.length)
       throw new NotFoundException(`No questions found.`)
@@ -42,25 +42,42 @@ export class QuestionService {
   }
 
   async findByKeywordsUser(Userid : number): Promise<Object[]>{
-    /* const keywrd=await this.manager.findOne(Keyword,keyword, {relations:["questions"]})
-     if (!keywrd)
-       throw new NotFoundException(`No questions with keyword "${keyword}" found.`)
-     return keywrd.questions
-     */
+
     const quest= await this.keywordRepository.createQueryBuilder("Keyword").leftJoin("Keyword.questions", "questions").where(`questions.Userid=${Userid}`).loadRelationCountAndMap('Keyword.questionCount', 'Keyword.questions').take(10).getMany()
+
     if (!quest || !quest.length)
       throw new NotFoundException(`No questions for user with id "${Userid}" found.`)
     return quest
   }
 
-  /*
-  update(id: number, updateQuestionDto: UpdateQuestionDto) {
-    return `This action updates a #${id} question`;
+  async showPerDay() : Promise<Object[]>{
+    const d_to = new Date();
+    const date = d_to.toISOString();
+
+    const quest = await createQueryBuilder().select(`EXTRACT(DAY FROM date_created),COUNT(*)` ).from('question', 'Question').andWhere(`date_created <= '${date}'`).andWhere(`date_created >= '${(addMonths(d_to, -1)).toISOString()}'`).groupBy(  `EXTRACT(DAY FROM date_created)`).orderBy('count', 'DESC').take(10).getRawMany()
+
+    //const quest = await this.questionRepository.query(`SELECT ARRAY_AGG(question.id) AS questions, * from Question WHERE date_created <= '${date}' AND date_created >= '${(addMonths(d_to, -1)).toISOString()}' GROUP BY EXTRACT(DAY FROM date_created)`)
+    //const quest = await this.questionRepository.query(`SELECT COUNT(qu) ,EXTRACT(DAY FROM date_created) AS day from Question WHERE date_created <= '${date}' AND date_created >= '${(addMonths(d_to, -1)).toISOString()}' GROUP BY EXTRACT(DAY FROM date_created)`)
+   // const quest = await entityManager.query(`SELECT * FROM question`)
+    if (!quest || !quest.length)
+      throw new NotFoundException(`No questions found ths last month.`)
+    return quest
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} question`;
+
+  async showPerDayUser(Userid:number) : Promise<Object[]>{
+    const d_to = new Date();
+    const date = d_to.toISOString();
+    const quest = await createQueryBuilder().select(`EXTRACT(DAY FROM date_created) AS day,COUNT(*)` ).from('question', 'Question').andWhere(`date_created <= '${date}'`).andWhere(`date_created >= '${(addMonths(d_to, -1)).toISOString()}'`).andWhere(`Question.Userid=${Userid}`).groupBy(  `EXTRACT(DAY FROM date_created)`).orderBy('count', 'DESC').take(10).getRawMany()
+    if (!quest || !quest.length)
+      throw new NotFoundException(`No questions found ths last month for user with id ${Userid}.`)
+    return quest
   }
 
-   */
+  async getMostPopular(): Promise<Question[]>{
+    const questions=this.manager.find(Question, {order: {sum_answers : "ASC"}, take : 10})
+    if (!questions)
+      throw new NotFoundException('No questions found')
+    return questions
+  }
 }
