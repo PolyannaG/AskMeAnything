8832e97886b1e048a6f70f5769fb2b3859e67070
PolyannaG <poll.go@hotmail.com>
2021-05-22 21:29:32 +0300

    change in migrations

---
+++ Microservices/manage-users/src/migration/1621707622163-InitialSchema.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class InitialSchema1621707622163 implements MigrationInterface {
+    name = 'InitialSchema1621707622163'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "manage_users"."user" ("id" SERIAL NOT NULL, "username" character varying(40) NOT NULL, "password" character varying NOT NULL, "email" character varying(40) NOT NULL, "user_since" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_315e576e3c8316d1224b32da4b7" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "manage_users"."user"`);
+    }
+
+}
