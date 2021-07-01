9ca1ba547c01b993a87af826b8dfc692688e9f56
PolyannaG <poll.go@hotmail.com>
2021-07-01 14:31:43 +0300

    client (old frontend folder) deleted

--- client/src/sample_data/user_dummy_dta.js
+++
@@ -1,9 +0,0 @@
-export const dummy_user = [
-    {
-    username : 'myUserName',
-    email: 'MyEmail@gmail.com',
-    user_since : '02-08-2020',
-    answers : '250',
-    questions : '117'
-    }
-]
